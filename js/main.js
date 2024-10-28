6af5937d806f337ad3f787f539428b9e689f2e80
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-28 17:28:30 +0400

    feat: add project

---
+++ js/main.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+   // main function
+   (()=>{
+      const photos = {
+
+         // vars
+         value:'food',
+         inputSearch: document.querySelector('#search'),
+         form: document.querySelector('.fetch-form'),
+         imagesHolder: document.querySelectorAll('.photos__item img'),
+         result:'',
+         choiceBtn: document.querySelectorAll('[data-info]'),
+         
+         // =============functions
+         // get images from api
+         getImages: async function () {
+
+            if(this.value){
+               
+               try{
+
+               const call = await fetch (`https://api.unsplash.com/search/photos?client_id=WGkjS5dARnRF_xWGDjrZftOS8kvQLgXbFpmaiWJOuic&query=${this.value}&per_page=12&orientation=portrait`);
+
+               let answer = await call.json();
+
+               this.result = answer.results;
+
+               this.renderImages();
+               }catch{
+
+                  alert('Тебе не нужна эта картинка. Поищи что-нибудь другое...');
+                  
+               }
+            }else{
+               alert('ты серьёзно?');
+            }
+         },
+
+         // render
+         renderImages: function(){
+            for(let i = 0; i < this.imagesHolder.length; i++){
+               this.imagesHolder[i].src = this.result[i].urls.regular;
+            }
+         },
+
+         // addForm
+         addForm: function (){
+            this.form.addEventListener('submit',(e) =>{
+               e.preventDefault();
+               this.value = this.inputSearch.value;
+               this.getImages();
+            });
+         },
+         
+         // send enter
+         sendEnter: function(){ 
+            this.inputSearch.addEventListener('keydown', (e) => {
+                if (e.key === 'Enter') {
+                e.preventDefault();
+                this.value = this.inputSearch.value;
+                this.getImages();
+               }
+            })
+         },
+
+         // choice buttons
+         choiceRequest: function(){
+            this.choiceBtn.forEach((item)=>{
+               item.addEventListener('click',(e)=>{
+                  this.inputSearch.value = item.dataset.info;
+               })
+            })
+         }
+
+      }
+
+      photos.choiceRequest();
+      photos.getImages();
+      photos.addForm();
+      photos.sendEnter();
+
+   })();
+
+   // mobile version
+   (()=>{
+      document.querySelector('.burger').addEventListener('click',()=>{
+         document.querySelector('.menu-choice').classList.toggle('active');
+         document.querySelector('.burger').classList.toggle('active');
+      })
+   })();
+});
+
+
